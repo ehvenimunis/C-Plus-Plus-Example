1069435aec3cc8d3816c3ec9a8bfd333405d0c40
ehvenimunis <muhammedsalihaydogan@gmail.com>
2022-05-03 17:01:37 +0300

    Pointer and constant

--- C++/Ders9  Pointer and Constant/src/Ders9.cpp
+++ C++/Ders9  Pointer and Constant/src/Ders9.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 void printarray(const int *ptr1, const int *ptr2){ // const olduğu için elemanın değeri değişir ama adresi değişemez!
 	for(; ptr1 != ptr2; ptr1++){
-		cout << "Eleman : " << *ptr1 << endl;
+		cout << "Eleman : " << *ptr1 << endl << endl;
 	}
 }
 
@@ -20,6 +20,84 @@ int main() {
 
 	int a[] = {10, 20, 30, 40, 50, 60, 70, 80, 90};
 	printarray(a+2, a+6);
+
+
+
+	// Pointers to constant:
+
+    int high{ 100 };
+    int low{ 66 };
+    const int* score{ &high };
+
+    // Pointer variable are read from
+    // the right to left
+    cout << *score << "\n";
+
+    // Score is a pointer to integer
+    // which is constant *score = 78
+
+    // It will give you an Error:
+    // assignment of read-only location
+    // ‘* score’ because value stored in
+    // constant cannot be changed
+    score = &low;
+
+    // This can be done here as we are
+    // changing the location where the
+    // score points now it points to low
+    cout << *score << "\n";
+
+
+
+    //Constant pointers
+    cout << endl;
+    int ab{ 90 };
+    int bb{ 50 };
+
+    int* const ptrt{ &ab };
+    cout << *ptrt << "\n";
+    cout << ptrt << "\n";
+
+    // Address what it points to
+
+    *ptrt = 56;
+
+    // Acceptable to change the
+    // value of a
+
+    // Error: assignment of read-only
+    // variable ‘ptrt’
+    // ptrt = &bb;
+
+    cout << *ptrt << "\n";
+    cout << ptrt << "\n";
+
+
+
+
+    // Constant Pointers to constants
+
+    const int aa{ 50 };
+     const int ba{ 90 };
+
+     // ptr points to a
+     const int* const ptra{ &aa };
+
+     // *ptr = 90;
+     // Error: assignment of read-only
+     // location ‘*(const int*)ptra’
+
+     // ptr = &b;
+     // Error: assignment of read-only
+     // variable ‘ptr’
+
+     // Address of a
+     cout << endl <<  ptra << "\n";
+
+     // Value of a
+     cout << *ptra << "\n";
+
+
 
 	return 0;
 }
